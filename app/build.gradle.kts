5fa62ec34c57aab9f56732f801708a34589d781f
isaico <nikita.gybacheff@gmail.com>
2026-06-25 22:32:06 +0500

    260623 - M3Exp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,4 +85,5 @@ dependencies {
     ksp("com.github.MatrixDev.Roomigrant:RoomigrantCompiler:0.3.4")
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    implementation("com.github.mmin18:RealtimeBlurView:1.2.1")
 }
